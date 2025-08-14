a28402d21ea8735cf4979dfdc9cbbcd0fc391fac
Bianca499 <bianca.s.silva20@aluno.senai.br>
2025-08-14 11:26:16 -0300

    Add files via upload

---
+++ Lista2.java
@@ -0,0 +1,82 @@
+package Primeiro;
+
+import java.util.Scanner;
+
+public class Lista2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//Resutado de dous numeros;
+		int numero1;
+		int numero2;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Inforne um numero");
+		numero1 = scanner.nextInt();
+		System.out.println("Inforne outro numero");
+		numero2 = scanner.nextInt();
+		
+		int soma = numero1+ numero2;
+		int substacao = numero1 - numero2;
+		int multiplicacao  = numero1 * numero2;
+		int divisao = numero1 / numero2;
+		System.out.println("A soma desses numeros resuta em:" + soma +"; a subtacao");
+		
+		//Calculo de temperatura:
+		double celsius;
+		System.out.println("Inforne a temperatura em celsius :");
+		Celsius = scanner.nexDouble();
+		double fahrenheit =(celsius *1.8).scanner();
+		System.out.println("Informa a temperatura de celis");
+		
+		//Conversao de valor
+		double dolar;
+		System.out.println("Inforne o valor em dolares");
+		dolar = scanner.nextDouble();
+		double real;
+		real = scanner.nextDouble();
+		double conversao;
+		System.out.println();
+		
+		// Areia do retangulo:
+		double triangulo;
+		System.out.println("Inforne a basse e a altura do triangulo");
+		Base = scanner.nexDouble();
+		System.out.println("Infonrne a altura da basse ");
+		
+		
+		// Conversor da idade 
+		double idade 
+		Systen.out.printlh("Qual e a sua idade");
+		System.out.printlh("escolha um numero ");
+		
+		
+		// Conversor de minuitos para segundos 
+		double minuito 
+		double horAS
+		
+		
+		
+		
+		
+		// CUSTO DE COMPRA 
+		double dinheiro 
+		da aim 
+		
+		
+		//Divisao de uma compra de restaurande 
+		
+double em dinheiro 
+
+
+
+//calculo de troco 
+exemplo 		
+		
+		
+		
+		
+
+		
+		
+		
