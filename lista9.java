2e6088cf1e3fdbb70054b13a8e7cd5cdd0f0ce76
Bianca499 <bianca.s.silva20@aluno.senai.br>
2025-09-12 14:18:54 -0300

    Add files via upload

---
+++ lista9.java
@@ -0,0 +1,16 @@
+package Primeiro;
+
+public class lista9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Exercio 1*/
+		public class  {
+		    public static void main1(String[] args) {
+		        int i = 1;
+		        while (i <= 10) {
+		            System.out.println(i);
+		        }
+
+}
+		}
