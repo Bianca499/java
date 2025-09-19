b58ab529990455663f9f5fd8670182765d66bb2e
Bianca499 <bianca.s.silva20@aluno.senai.br>
2025-09-19 08:27:13 -0300

    Add files via upload

---
+++ Lista13.java
@@ -0,0 +1,77 @@
+package Primeiro;
+
+import java.awt.SystemColor;
+import java.util.Scanner;
+
+public class Lista13 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in); //java.util
+		// 1 - Contador: Mostre os números de 10 a 30 no console
+		for (int cont= 10; cont <= 30; cont++) {
+		System.out.println(cont);
+		}
+		System.out.println("------------");
+		
+		// 2- classificação de idades
+		int idade;
+		for(int cont=1; cont <=10; cont++) {
+			System.out.println("informe a sua idade");
+			idade = scanner.nextInt();
+			
+			if(idade < 18) {
+				System.out.println("Menor de idade");
+			} else {
+				System.out.println("Maior de idade");
+			}
+			//3- Eleição
+			int eleiçao;
+			for(int cont1= 1; cont1< 5; cont1++) {
+				System.out.println("Você voutou no cditrado");
+			System.out.println ("eleicao ==5") ;
+				System.out.println("Voto Nulo");
+				System.out.println("eleicao ==-5");
+				System.out.println("voto em branc0 "
+				System.out.println("eleicao ==-6");
+			 
+				//4- Repetição de Frase
+				System.out.println("escolhe um numero");
+				int numero;
+				System.out.println("informe o numero");
+				
+				//5- Sequencia 
+				System.out.println("escolhe  um numero");
+				int numero1;
+			
+				//6-Numeros Alternados
+				System.out.println("moste um numero 1 a 50");
+				System.out.println("pule em 2 em dois");
+				
+				//7-Aprovado ou reprovado
+				System.out.println("fala 5 notas");
+				System.out.println("Para cada nota informe se o aluno foi Aprovado ou reprovado");
+				
+				//8-categoria de idade
+				System.out.println("fala a idade 5 pessoas");
+				int crianca;
+				System.out.println("ate 12:crianca");
+				int adolecente;
+				System.out.println("13 a 17:Adolecente");
+				int adulto;
+				System.out.println("18 a 59:aulto");
+				int idolso;
+				System.out.println("60+:idolso");
+			}
+			}
+			}
+		}
+		
+		  
+		
+
+	
+		  
+		  
+		
+
+	
